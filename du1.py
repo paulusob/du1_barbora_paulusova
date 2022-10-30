562b8f5a264cc049976b388f9d8819fa660b5f98
Barbora Paulusová <paulusob@natur.cuni.cz>
2022-10-30 22:16:29 +0100

    s popisky

--- du1.py
+++ du1.py
@@ -1,24 +1,25 @@
+#nejdříve je potřeba naimportovat příkazy z knihovny turtle
 from turtle import forward, exitonclick, right, left, speed, setx, sety, setpos, circle, write, penup, pendown, home
+#spuštění hry, vysvětlení pravidel
 print("Vítejte ve hře piškvorky")
 print("Hráč 1 má symbol kolečka, hráč 2 má symbol křížku, Na vyzvání zadejte nejdříve souřadnici x, posléze souřadnici y vašeho tahu")
-
-
-#vykresleni ctvercove site 
+#vykreslení čtvercové sítě, zatím o velikosti 3*3
 a=3
 b=3
 strana=100
-speed(0)
+speed(0) 
 for k in range(b):
     for j in range(a):
-        for i in range(4):
+        for i in range(4): #vykreslí čtverec 
             forward(strana)
             left(90)
-        forward(strana)
+        forward(strana) #vykreslí řadu čtverců
     right(180)
     forward(a*strana)
     right(90)
     forward(strana)
     right(90)
+#popisky hracího pole - osa x,y a interval (1,3)
 penup()
 setx(310)
 sety(-5)
@@ -62,13 +63,16 @@ write ("3",move=False, align='left',font=('Arial',8,'normal'))
 penup()
 home()
 pendown()
-for l in range (4):
+#začátek hry 
+#v hracím poli je dohromady 9 políček - hráč 1 má tedy 5 tahů, hráč 2 má 4 tahy, poslední tah prvního hráče je uveden zvlášť
+for l in range (4): #4 tahy jsou pro oba hráče společné 
     print("Je na tahu hráč 1")
-    x=float(input("Zadejte x souřadnici v intervalu (1-3): "))
-    while x>3 or x<1:
+    x=float(input("Zadejte x souřadnici v intervalu (1-3): ")) #hráč zadá souřadnice, na které se vykreslí znak
+    while x>3 or x<1: #podmínka, že x (resp. y) musí být v inervalu (1,3) podle nákresu hracího pole
+        # - pokud není splněna, uživatel je vyzván k zadání jiného čísla v intervalu (1,3)
         print("x musí být v intervalu (1,3)")
         x=float(input("Zadejte znovu x souřadnici v intervalu (1-3): "))
-    if x==1:
+    if x==1: #převedení relativních souřadnic hracího pole na absolutní souřadnice
         x=0
     elif x==2:
         x=100
@@ -84,13 +88,16 @@ for l in range (4):
         y=100
     else:
         y=200
-    setx(x)
-    sety(y)
-    forward (50)
-    circle(50)
-    left(180)
+    #nastavení souřadnic, na kterých se bude kreslit symbol
+    setx(x) 
+    sety(y)  
+    #vykreslení kruhu
+    forward (50) 
+    circle(50) 
+    left(180) 
     forward(50)
     left(180)
+    #konec tahu prvního hráče, je na řadě hráč 2, proces je stejný jako u hráče 1
     print("Je na tahu hráč 2")
     x=float(input("Zadejte x souřadnici (1-3): "))
     while x>3 or x<1:
@@ -114,6 +121,7 @@ for l in range (4):
         y=200
     setx(x)
     sety(y)
+    #vykreslení křížku
     left(45)
     forward (141.42)
     left(135)
@@ -123,6 +131,7 @@ for l in range (4):
     right(135)
     forward(100)
     left(180)
+#poslední tah prvního hráče
 print("Je na tahu hráč 1")
 x=float(input("Zadejte x souřadnici (1-3): "))
 while x>3 or x<1:
--- testy.py
+++ testy.py
@@ -1,12 +1,25 @@
-from turtle import forward, exitonclick, right, left, speed, circle, setx, sety
+from turtle import forward, exitonclick, right, left, speed, circle, setx, sety, setpos 
+
+#souradnice
+for i in range(4):
+	forward(100)
+	left(90)
+setx(100)
+sety(100)
+forward (50)
+circle(50)
+left(180)
+forward(50)
+left(180)
+exitonclick()
 
 #kolecka 
-#for i in range(4):
-#	forward(100)
-#	left(90)
-#forward (50)
-#circle(45)
-#exitonclick()
+for i in range(4):
+	forward(100)
+	left(90)
+forward (50)
+circle(50)
+exitonclick()
 
 #krizky 
 for i in range(4):
@@ -21,3 +34,23 @@ forward(141.42)
 right(135)
 forward(100)
 exitonclick()
+
+#ctvercova sit 
+#a=3
+#b=3
+#strana=100
+#speed(4)
+#for k in range(b):
+	# Nakreslí řádek
+	#for j in range(a):
+		# Nakreslí čtverec
+		#for i in range(4):
+		#	forward(strana)
+		#	left(90)
+		#forward(strana)
+	#right(180)
+	#forward(a*strana)
+	#right(90)
+	#forward(strana)
+	#right(90)
+#exitonclick()
