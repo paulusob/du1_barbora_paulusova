dafa8d6757cbefc51d1c5212e6f85394fac646af
Barbora Paulusová <paulusob@natur.cuni.cz>
2022-10-30 16:55:40 +0100

    kresli kolecka na souradnice

--- du1.py
+++ du1.py
@@ -2,16 +2,17 @@ from turtle import forward, exitonclick, right, left, speed, setx, sety, setpos,
 print("Vítejte ve hře piškvorky")
 print("Hráč 1 má symbol křížku, hráč 2 má symbol kolečka, Na vyzvání zadejte nejdříve souřadnici x, posléze souřadnici y vašeho tahu")
 print("Je na tahu hráč 1")
-x=input("Zadejte x souřadnici: ")
-y=input("Zadejte y souřadnici: ")
+
+x=float(input("Zadejte x souřadnici: "))
+y=float(input("Zadejte y souřadnici: "))
 #vykresleni ctvercove site 
 a=3
 b=3
 strana=100
-speed(4)
-for y in range(b):
+speed(9)
+for k in range(b):
 	# Nakreslí řádek
-	for x in range(a):
+	for j in range(a):
 		# Nakreslí čtverec
 		for i in range(4):
 			forward(strana)
@@ -30,5 +31,3 @@ left(180)
 forward(50)
 left(180)
 exitonclick()
-
-
