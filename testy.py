36df1dfd06e7c4e055c2ab29a01064147b9bc07f
Barbora Paulusová <paulusob@natur.cuni.cz>
2022-10-30 16:03:13 +0100

    kolecka

---
+++ testy.py
@@ -0,0 +1,10 @@
+from turtle import forward, exitonclick, right, left, speed, circle, setx, sety
+
+#kolecka 
+for i in range(4):
+	forward(100)
+	left(90)
+forward (50)
+circle(45)
+exitonclick()
+
