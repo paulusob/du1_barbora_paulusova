8fe7784fe1b38908d29b02c233f280783c9dd69c
Barbora Paulusová <paulusob@natur.cuni.cz>
2022-10-30 16:13:24 +0100

    kresli kryzky

--- testy.py
+++ testy.py
@@ -1,10 +1,23 @@
 from turtle import forward, exitonclick, right, left, speed, circle, setx, sety
 
 #kolecka 
+#for i in range(4):
+#	forward(100)
+#	left(90)
+#forward (50)
+#circle(45)
+#exitonclick()
+
+#krizky 
 for i in range(4):
 	forward(100)
 	left(90)
-forward (50)
-circle(45)
+left(45)
+forward (141.42)
+left(135)
+forward(100)
+left(135)
+forward(141.42)
+right(135)
+forward(100)
 exitonclick()
-
